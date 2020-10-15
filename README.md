8b194259106469145889dc90e5a9284875d08910
Adam McCurdy <adam@mothandflamevr.com>
2020-10-15 02:01:01 -0700

    improved error handling for main 3 errors

--- README.md
+++ README.md
@@ -7,14 +7,14 @@ A simple [Visual Studio Code](https://code.visualstudio.com/) extension to lint 
 2. Make linter work on type instead of save only. 
 3. Make #2 an option
 4. Make the highlighted text the actual problem, not what follows problem.
+5. fix unexpected identifier bug
 
 ## Requirements
 1. Clone the linter `git clone https://github.com/Makopo/lslint.git`
 2. Install it in your system path
 3. Ensure that `lslint` is installed in your path, by opening up powershell or cmd or terminal and typing `lslint`;
 4. Run [`Install Extension`](https://code.visualstudio.com/docs/editor/extension-gallery#_install-an-extension) command from [Command Palette](https://code.visualstudio.com/Docs/editor/codebasics#_command-palette).
-5. Search and choose `lslint`.
-6. Change icon
+5. Search and choose `lslint`
 
 ## Options
 `"lslint.enable"` - enable LSL linter
@@ -26,6 +26,7 @@ Default options are:
 {
     "lsllinter.enable": true,
     "lsllinter.warnOnSave": false,
+    "lsllinter.realtimeLinting": true,
 }        
 ```
 
--- src/extension.ts
+++ src/extension.ts
@@ -3,10 +3,6 @@
 import * as path from "path";
 import * as vscode from "vscode";
 import {spawn, ChildProcess} from "child_process";
-// import { pathToFileURL } from "url";
-// import { print } from "util";
-
-// const OUTPUT_REGEXP = /([0-9]+): (.+) near.*[<'](.*)['>]/;
 
 let diagnosticCollection: vscode.DiagnosticCollection;
 let currentDiagnostic: vscode.Diagnostic;
@@ -17,18 +13,16 @@ function generateErrorMatches(payload: string){
 
     allLines.forEach(function(item, index, object) {
         if(item.includes("ERROR::")){
-            //lineArray build as name 0, line 1, text 2, at 3
             let numbers = item.match(/\d+/g);
-            let text = item.match(/'((?:''|[^'])*)'/)[0].split('\'').join("");                    
-            parsedLines.push({name: "Error", line: parseInt(numbers[0]), text: text, at: parseInt(numbers[1])})
+            parsedLines.push({name: "Error", line: parseInt(numbers[0]) - 1, text: " ", at: parseInt(numbers[1])});
         }
-    });    
+    });
     return parsedLines;
 }
 
 function parseDocumentDiagnostics(document: vscode.TextDocument, lslcOutput: string) {
     // const matches = OUTPUT_REGEXP.exec(lslcOutput);
-    const errors = generateErrorMatches(lslcOutput); 
+    const errors = generateErrorMatches(lslcOutput);
     errors.forEach(matches => {
         const message = {
             line: matches["line"],
@@ -39,7 +33,7 @@ function parseDocumentDiagnostics(document: vscode.TextDocument, lslcOutput: str
         if (!message.line) {
             return;
         }
-    
+
         var errorLine = document.lineAt(message.line - 1).text;
         var rangeLine = message.line - 1;
         var rangeStart = new vscode.Position(rangeLine, 0);
@@ -70,9 +64,9 @@ function lintDocument(document: vscode.TextDocument, warnOnError: Boolean = true
 
     let filePath = vscode.window.activeTextEditor.document.uri.fsPath;
     // Determine the interpreter to use
-    let interpreter = lsllinterConfig.get<string>("interpreter");    
+    let interpreter = lsllinterConfig.get<string>("interpreter");
     var lslProcess: ChildProcess = spawn(interpreter, [filePath]);
-    
+
     lslProcess.stdout.setEncoding("utf8");
 
     lslProcess.stderr.on("data", (data: Buffer) => {
@@ -81,7 +75,7 @@ function lintDocument(document: vscode.TextDocument, warnOnError: Boolean = true
         }
         parseDocumentDiagnostics(document, data.toString());
     });
-    
+
     lslProcess.stderr.on("error", error => {
         vscode.window.showErrorMessage(interpreter + " error: " + error);
     });
@@ -104,6 +98,8 @@ export function activate(context: vscode.ExtensionContext) {
     diagnosticCollection = vscode.languages.createDiagnosticCollection('lsl');
     context.subscriptions.push(diagnosticCollection);
 
+
+    //TODO: Make this didchangetextdocument work
     vscode.workspace.onDidSaveTextDocument(document => lintDocument(document, true));
     vscode.workspace.onDidChangeTextDocument(event => lintDocument(event.document));
     vscode.workspace.onDidOpenTextDocument(document => lintDocument(document));
